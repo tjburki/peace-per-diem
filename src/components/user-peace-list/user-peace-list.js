ff6b8da92e354e4c53ca8865b43cb982e7e8a636
Tyler Burki <TBurki@blainsupply.com>
2020-04-18 21:22:03 -0500

    Adding a cancel to the update to allow user to return to initial state

--- src/components/user-peace-list/user-peace-list.js
+++ src/components/user-peace-list/user-peace-list.js
@@ -12,10 +12,12 @@ const UserPeaceList = () => {
     const { user } = useAuth0();
     const defaultTodaysPeace = { text: '', user_id: user.user_id };
 
+    const [todaysPeaceDraft, setTodaysPeaceDraft] = useState('');
     const [todaysPeace, setTodaysPeace] = useState(defaultTodaysPeace);
     const [oldPeaces, setOldPeaces] = useState([]);
     const [loading, setLoading] = useState(false);
     const [editing, setEditing] = useState(false);
+    const [initialized, setInitialized] = useState(false);
 
     async function callGetPeacesForUser() {
         setLoading(true);
@@ -26,6 +28,7 @@ const UserPeaceList = () => {
         setTodaysPeace(todaysPeaceToSet);
         setOldPeaces(userPeaces);
         setEditing(!todaysPeaceToSet.text);
+        setInitialized(true);
         setLoading(false);
     }
 
@@ -34,24 +37,56 @@ const UserPeaceList = () => {
     }, []);
 
     async function callCreateUpdatePeace() {
-        await createUpdatePeace(todaysPeace);
+        await createUpdatePeace({ ...todaysPeace, text: todaysPeaceDraft });
         callGetPeacesForUser();
+    }
+
+    function peaceChange(e) {
+        const text = e.target.value;
+        setTodaysPeaceDraft(text);
+    }
+
+    function submitPeace(e) {
+        e.preventDefault();
+        callCreateUpdatePeace();
+    }
+
+    function edit() {
+        setTodaysPeaceDraft(todaysPeace.text);
+        setEditing(true);
+    }
+
+    function cancelEdit() {
+        setTodaysPeaceDraft(''); //Probably unnecessary
+        setEditing(false);
     }
 
     if (loading) return <GiganticText><Loading /></GiganticText>;
 
     return (
         <div>
-            <div className={styles.todayspeace}>      
+            <div className={styles.todayspeace}>   
+                <div className={styles.todayspeacetitle}>
+                    <div>Today's Peace</div>
+                    <div className={styles.editcontrol}>
+                        {
+                            editing && todaysPeace.text
+                                ?   <span onClick={cancelEdit} title='cancel'><Icon type='times' /></span>
+                                :   editing || loading || !initialized
+                                    ?   null
+                                    :   <span onClick={edit} title='edit'><Icon type='edit' /></span>
+                        }
+                    </div>
+                </div>   
                 {
                     editing
-                        ?   <form className={styles.form} onSubmit={e => {e.preventDefault(); callCreateUpdatePeace();}}>
+                        ?   <form className={styles.form} onSubmit={submitPeace}>
                                 <textarea 
                                     rows='6' 
                                     required 
                                     maxLength={255} 
-                                    value={todaysPeace.text} 
-                                    onChange={e => { const text = e.target.value; setTodaysPeace({...todaysPeace, text}); }} 
+                                    value={todaysPeaceDraft} 
+                                    onChange={peaceChange} 
                                     className='form-control' 
                                     placeholder='Something that has inspired you or given you hope today'
                                 > 
@@ -61,12 +96,6 @@ const UserPeaceList = () => {
                                 </button>
                             </form>
                         :   <div className={styles.todayspeacedisplay}>
-                                <div className={styles.todayspeacetitle}>
-                                    <div>Today's Peace</div>
-                                    <div className={styles.editicon} onClick={() => setEditing(true)} title='edit'>
-                                        <Icon type='edit' />
-                                    </div>
-                                </div>
                                 <Peace text={todaysPeace.text} />
                             </div>
                 }
